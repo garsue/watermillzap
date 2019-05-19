a75b07014fda289ba57ee58c51df4f537f14778f
garsue <osuga.shunya@mfkessai.co.jp>
2019-05-20 00:52:04 +0900

    Write doc comments

--- adapter.go
+++ adapter.go
@@ -1,3 +1,4 @@
+//Package watermillzap provides the implementation of watermill.LoggerAdapter using go.uber.org/zap.
 package watermillzap
 
 import (
@@ -5,15 +6,18 @@ import (
 	"go.uber.org/zap"
 )
 
+// Logger implements watermill.LoggerAdapter with *zap.Logger.
 type Logger struct {
 	backend *zap.Logger
 	fields  watermill.LogFields
 }
 
+// NewLogger returns new watermill.LoggerAdapter using passed *zap.Logger as backend.
 func NewLogger(z *zap.Logger) watermill.LoggerAdapter {
 	return &Logger{backend: z}
 }
 
+// Error writes error log with message, error and some fields.
 func (l *Logger) Error(msg string, err error, fields watermill.LogFields) {
 	fields = l.fields.Add(fields)
 	fs := make([]zap.Field, 0, len(fields)+1)
@@ -24,6 +28,7 @@ func (l *Logger) Error(msg string, err error, fields watermill.LogFields) {
 	l.backend.Error(msg, fs...)
 }
 
+// Info writes info log with message and some fields.
 func (l *Logger) Info(msg string, fields watermill.LogFields) {
 	fields = l.fields.Add(fields)
 	fs := make([]zap.Field, 0, len(fields)+1)
@@ -33,6 +38,7 @@ func (l *Logger) Info(msg string, fields watermill.LogFields) {
 	l.backend.Info(msg, fs...)
 }
 
+// Debug writes debug log with message and some fields.
 func (l *Logger) Debug(msg string, fields watermill.LogFields) {
 	fields = l.fields.Add(fields)
 	fs := make([]zap.Field, 0, len(fields)+1)
@@ -42,6 +48,7 @@ func (l *Logger) Debug(msg string, fields watermill.LogFields) {
 	l.backend.Debug(msg, fs...)
 }
 
+// Trace writes debug log instead of trace log because zap does not support trace level logging.
 func (l *Logger) Trace(msg string, fields watermill.LogFields) {
 	fields = l.fields.Add(fields)
 	fs := make([]zap.Field, 0, len(fields)+1)
@@ -51,6 +58,7 @@ func (l *Logger) Trace(msg string, fields watermill.LogFields) {
 	l.backend.Debug(msg, fs...)
 }
 
+// With returns new LoggerAdapter with passed fields.
 func (l *Logger) With(fields watermill.LogFields) watermill.LoggerAdapter {
 	return &Logger{
 		backend: l.backend,
